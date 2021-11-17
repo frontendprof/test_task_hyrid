eb933e43126f26c9757073dc3668abdf8426718e
frontendprof <https://github.com/frontendprof>
2021-11-17 05:08:00 +0500

    products section fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,32 @@ window.onscroll = () => {
   if (this.scrollY >= 200) nav.classList.add('scroll-header');
   else nav.classList.remove('scroll-header');
 };
+
+// Lazy Loading
+const lazyImgs = document.querySelectorAll('img[data-src]');
+// const labelAll = document.querySelectorAll('product_label_shipping');
+function preloadImg(img) {
+  const src = img.getAttribute('data-src');
+  if (!src) {
+    return;
+  }
+  img.src = src;
+}
+const imgOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px -500px 0px',
+};
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImg(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+lazyImgs.forEach((img) => {
+  imgObserver.observe(img);
+});
